7a42d81a64cd04ddc52f19d82ccc1f2fc633df2e
Brad Chao <brad@brad.tw>
2016-12-01 15:18:37 +0800

    v9

--- app/src/main/java/com/example/user/brad28/F1.java
+++ app/src/main/java/com/example/user/brad28/F1.java
@@ -9,23 +9,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class F1 extends Fragment {
+    private TextView mesg;
+    private String strLottery = null;
     public static F1 newInstance() {
         F1 fragment = new F1();
         return fragment;
     }
 
+    public F1(){
+        Log.v("brad", "F1()");
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("brad", "onCreate");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.v("brad", "onCreateView");
         View view = inflater.inflate(R.layout.fragment_f1, container, false);
+        mesg = (TextView)view.findViewById(R.id.f1mesg);
+        if (strLottery != null){
+            mesg.setText(strLottery);
+        }
+
+
         View btn = view.findViewById(R.id.f1btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +51,8 @@ public class F1 extends Fragment {
         return view;
     }
     private void doBtn(){
-        Log.v("brad", "F1 Button");
+        strLottery = "Lottery: " + (int)(Math.random()*49+1);
+        mesg.setText(strLottery);
     }
 
 }
