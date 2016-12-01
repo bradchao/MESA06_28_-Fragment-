513616bc46efd86eb6be744f4a56df6ce2e4cdbe
Brad Chao <brad@brad.tw>
2016-12-01 14:02:56 +0800

    v2

---
+++ app/src/main/java/com/example/user/brad28/F2.java
@@ -0,0 +1,27 @@
+package com.example.user.brad28;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 2016/12/1.
+ */
+
+public class F2 extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    
+}
